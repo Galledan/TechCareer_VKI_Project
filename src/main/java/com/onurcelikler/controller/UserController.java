f9e0e388a7eb94ec1d4ba58cddc70a7330f0983e
Galledan <onrclklr@outlook.com>
2023-11-08 17:49:14 +0300

    Added axios to React and created components, added cross origin to prevent CORS

--- src/main/java/com/onurcelikler/controller/UserController.java
+++ src/main/java/com/onurcelikler/controller/UserController.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
@@ -27,35 +28,35 @@ public class UserController {
 
     // Creating User
     @PostMapping("/users")
-    public User createUser(@RequestBody User user){
+    public User createUser(@RequestBody User user) {
         return userRepo.save(user);
     }
 
     // Getting User By Id
-@GetMapping("/users/{id}")
+    @GetMapping("/users/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
-    User user = userRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not Found with Id: " + id));
-    return ResponseEntity.ok(user);
-}
+        User user = userRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not Found with Id: " + id));
+        return ResponseEntity.ok(user);
+    }
 
     //Updating User
-@PutMapping("/users/{id}")
-    public ResponseEntity<User> updateUser(Long id,@RequestBody User userDetails){
-    User user = userRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not Found with Id: " + id));
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> updateUser(Long id, @RequestBody User userDetails) {
+        User user = userRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not Found with Id: " + id));
 
-    user.setFirstName(userDetails.getFirstName());
-    user.setLastName(userDetails.getLastName());
-    user.setKgValue(userDetails.getKgValue());
-    user.setLengthValue(userDetails.getLengthValue());
+        user.setFirstName(userDetails.getFirstName());
+        user.setLastName(userDetails.getLastName());
+        user.setKgValue(userDetails.getKgValue());
+        user.setLengthValue(userDetails.getLengthValue());
 
-    User updatedUser = userRepo.save(user);
-            return ResponseEntity.ok(updatedUser);
+        User updatedUser = userRepo.save(user);
+        return ResponseEntity.ok(updatedUser);
 
     }
 
     // Deleting User
     @DeleteMapping("/users/{id}")
-    public ResponseEntity<Map<String, Boolean>> deleteUser(@PathVariable Long id){
+    public ResponseEntity<Map<String, Boolean>> deleteUser(@PathVariable Long id) {
         User user = userRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not Found with Id: " + id));
 
         userRepo.delete((user));
@@ -63,5 +64,4 @@ public class UserController {
         response.put("deleted", Boolean.TRUE);
         return ResponseEntity.ok(response);
     }
-
 }
